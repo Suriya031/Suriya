97730642ca6c9eae1c5d9c7324e7a9b4c50531b9
Suriya031 <ksuriya031@gmail.com>
2024-10-21 16:19:16 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,31 @@
+package operator;
+
+public class datatype {
+public static void main(String[] args) {
+		
+		byte no1 = 110;
+		System.out.println(no1);
+		
+		short no2 = 5;
+		System.out.println("Short no: "+no2);
+		
+		int no3 = 10;
+		System.out.println("Int no is : " +no3);
+		
+		long no4 = 123456789L;
+		System.out.println("Long no is :" +no4);
+		
+		String name = "Suriya";
+		System.out.println("My name is :"+ name);
+		
+		float no5 = 11.5f;
+		System.out.println("Decimal no: " +no5);
+		
+		double no6 = 10.123456789;
+		System.out.println("Double Decimal no :"+no6);
+		
+		boolean flag = true;
+		System.out.println("Boolean value is: "+flag);
+
+ }
+}
