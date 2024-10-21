ce50d711ae87952b080e544a39dffc2b24c1d73f
Suriya031 <ksuriya031@gmail.com>
2024-10-21 16:21:22 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String animals[]= {"Dog","Cat","cow","Horse","sheep","pig","Duck","Fish","Rabbit","Tiger"};
+		
+	    
+	      for(int  i=0;i<animals.length;i++)
+	{
+		  System.out.println(animals[i]);
+	}
+
+
+	}
+
+}
