72b0edd5c5b2ed5b7d5c1b396de89c79008ad65b
Suriya031 <ksuriya031@gmail.com>
2024-10-21 16:03:03 +0530

    Add files via upload

---
+++ operatordata.java
@@ -0,0 +1,26 @@
+package operator;
+
+public class operatordata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+
+		int y=20;
+
+		boolean res1=x>y;
+		boolean res2=x==y;
+		boolean res3=x>=y;
+		boolean res4=x<=y;
+		boolean res5=x!=y;
+		boolean res6=x<y;
+
+		System.out.println(res1);
+		System.out.println(res2);
+		System.out.println(res3);
+		System.out.println(res4);
+		System.out.println(res5);
+		System.out.println(res6);
+	}
+
+}
